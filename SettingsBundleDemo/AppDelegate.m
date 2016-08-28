a66ea3e621c5a9dbdf0689bf468bc5a98d21d134
iJasonY <iJasonY@users.noreply.github.com>
2016-08-28 16:14:54 +0800

    add JSPatch SDK

--- SettingsBundleDemo/AppDelegate.m
+++ SettingsBundleDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <JSPatch/JSPatch.h>
 
 @interface AppDelegate ()
 
@@ -30,16 +31,58 @@
   [[NSUserDefaults standardUserDefaults] setObject:githash
                                             forKey:@"githash_preference"];
 
+  [self setUpJSPatch];
+
   return YES;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
 }
 
+- (void)setUpJSPatch {
+
+  //这个是用来做条件下发的
+  [JSPatch setupUserData:@{ @"userId" : @"10000" }];
+  NSString *kJSPatchAppKey = @"";
+  [JSPatch startWithAppKey:kJSPatchAppKey];
+
+  //用来检测回调的状态，是更新或者是执行脚本之类的，相关信息，会打印在你的控制台
+  [JSPatch
+      setupCallback:^(JPCallbackType type, NSDictionary *data, NSError *error) {
+
+        NSLog(@"error-->%@", error);
+        switch (type) {
+        case JPCallbackTypeUpdate: {
+          NSLog(@"更新脚本 %@ %@", data, error);
+          break;
+        }
+        case JPCallbackTypeRunScript: {
+          NSLog(@"执行脚本 %@ %@", data, error);
+          break;
+        }
+        case JPCallbackTypeCondition: {
+          NSLog(@"条件下发 %@ %@", data, error);
+          break;
+        }
+        case JPCallbackTypeGray: {
+          NSLog(@"灰度下发 %@ %@", data, error);
+          break;
+        }
+        default:
+          break;
+        }
+      }];
+
+  //#ifdef DEBUG
+  [JSPatch setupDevelopment];
+  //#endif
+  [JSPatch sync]; // 每调用一次 +sync 就会请求一次后
+}
+
 - (void)readSettingValue {
   BOOL switc = [[NSUserDefaults standardUserDefaults]
       boolForKey:@"innernetswitch_preference"];
-    
+
   NSLog(@"--------%d", switc);
 }
 
